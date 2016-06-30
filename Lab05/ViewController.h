8eed3fefc72d8d08f04d426f15a512f3066fc76b
Martin Leon Preciado <Martin@macbook-air-de-martin.local>
2016-06-29 19:49:10 -0500

    Initial Commit

---
+++ Lab05/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lab05
+//
+//  Created by Martin Leon Preciado on 29/06/16.
+//  Copyright © 2016 MartinLP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Lab05/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Lab05
+//
+//  Created by Martin Leon Preciado on 29/06/16.
+//  Copyright © 2016 MartinLP. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
