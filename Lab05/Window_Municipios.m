806c913f2bb40deb84970ea8cd73ae95c2d9e769
Martin Leon Preciado <Martin@macbook-air-de-martin.local>
2016-06-30 23:12:06 -0500

    New window has been implemented

---
+++ Lab05/Window_Municipios.m
@@ -0,0 +1,37 @@
+//
+//  Window_Municipios.m
+//  Lab05
+//
+//  Created by Martin Leon Preciado on 30/06/16.
+//  Copyright © 2016 MartinLP. All rights reserved.
+//
+
+#import "Window_Municipios.h"
+
+@interface Window_Municipios ()
+
+@end
+
+@implementation Window_Municipios
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
