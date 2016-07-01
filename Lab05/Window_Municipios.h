bc7f299503de8b160a5cca8b0da0a1a3e6f313e5
Martin Leon Preciado <Martin@macbook-air-de-martin.local>
2016-06-30 23:25:40 -0500

    Second Window with table has been implemented

--- Lab05/Window_Municipios.h
+++ Lab05/Window_Municipios.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "CellHome.h"
 
 @interface Window_Municipios : UIViewController
 
--- Lab05/Window_Municipios.m
+++ Lab05/Window_Municipios.m
@@ -9,13 +9,20 @@
 #import "Window_Municipios.h"
 
 @interface Window_Municipios ()
-
+@property NSMutableArray *destinationTitles;
+@property NSMutableArray *destinationImgs;
 @end
 
 @implementation Window_Municipios
 
+/**********************************************************************************************/
+#pragma mark - Initialization methods
+/**********************************************************************************************/
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self initController];
     // Do any additional setup after loading the view.
 }
 
@@ -24,14 +31,48 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+//--------------------------------------------------------------------------------------
+- (void)initController {
+    self.destinationTitles   = [[NSMutableArray alloc] initWithObjects: @"Aguascalientes",@"Guanajuato",@"Jalisco",@"Michoacan",@"Zacatecas",nil];
+    
+    self.destinationImgs   = [[NSMutableArray alloc] initWithObjects: @"Aguascalientes.jpg",@"Guanajuato.jpg",@"Jalisco.jpg",@"Michoacan.jpg",@"Zacatecas.jpg",nil];
 }
-*/
+
+/**********************************************************************************************/
+#pragma mark - Table methods and delegates
+/**********************************************************************************************/
+
+- (NSInteger) numberOfSectionsInTableView: (UITableView *)tableView{
+    return 1;
+}
+//--------------------------------------------------------------------------------------
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection: (NSInteger) section
+{
+    return self.destinationTitles.count;
+}
+//--------------------------------------------------------------------------------------
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath: (NSIndexPath *) indexPath
+{
+    return 190; //Tamaño de la celda
+}
+//--------------------------------------------------------------------------------------
+- (UITableViewCell *) tableView: (UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    //Inicializacion de celdas.
+    CellHome *cell = (CellHome *) [tableView dequeueReusableCellWithIdentifier:@"CellHome"];
+    
+    if (cell==nil)
+    {
+        [tableView registerNib:[UINib nibWithNibName:@"CellHome" bundle:nil]
+        forCellReuseIdentifier:@"CellHome"];
+        cell= [tableView dequeueReusableCellWithIdentifier:@"CellHome"];
+    }
+    cell.lblEstado.text = self.destinationTitles[indexPath.row];
+    cell.imgEstado.image = [UIImage imageNamed:self.destinationImgs[indexPath.row]];
+    
+    
+    return cell;
+}
+
 
 @end
